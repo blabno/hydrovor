46581abfe45010923871142510034d33dd2420b9
mikoxp <olesmichalxp@tlen.pl>
2015-03-24 15:21:00 +0100

    Test

--- src/test/java/hydrovor/WellTest.java
+++ src/test/java/hydrovor/WellTest.java
@@ -1,5 +1,20 @@
 package hydrovor;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class WellTest {
+
+    @Test
+    public void getWaterIsCorect()
+    {
+
+        //give
+        Well well =new Well(1);
+        //when
+        int result =well.getWater(0);
+        Assert.assertEquals(1, result);
+
+    }
 
 }
