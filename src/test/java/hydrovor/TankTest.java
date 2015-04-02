79780cf95752c3de1d9738d9cc7728964f2a2105
mikoxp <olesmichalxp@tlen.pl>
2015-04-02 18:55:06 +0200

    Tank

--- src/test/java/hydrovor/TankTest.java
+++ src/test/java/hydrovor/TankTest.java
@@ -1,5 +1,61 @@
 package hydrovor;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class TankTest {
 
-}
+    @Rule
+    public ExpectedException expectedException= ExpectedException.none();
+
+    @Test
+    public void tank_maxValueLessThanZero_exception()
+    {
+        expectedException.expect(IllegalArgumentException.class);
+        new Tank(-1,1);
+    }
+    @Test
+    public void tank_valueLessThanZero_exception()
+    {
+        expectedException.expect(IllegalArgumentException.class);
+        new Tank(1,-1);
+    }
+    @Test
+    public void getWater_nonepostiveValue_returnZero()
+    {
+        Tank tank=new Tank(1,0);
+        int result=tank.getWater();
+        Assert.assertEquals(0,result);
+    }
+    @Test
+    public void getWater_postiveValue_returnOne()
+    {
+        Tank tank=new Tank(1,1);
+        int result=tank.getWater();
+        Assert.assertEquals(1,result);
+    }
+    @Test
+    public void addWater_addLessThanZero_exception()
+    {
+        expectedException.expect(IllegalArgumentException.class);
+        Tank tank=new Tank(1,1);
+        int result=tank.addWater(-1);
+    }
+    @Test
+    public void addWater_complementTank_valuecomplement()
+    {
+        Tank tank=new Tank(2,1);
+        int result=tank.addWater(2);
+        Assert.assertEquals(1,result);
+
+    }
+    @Test
+    public void addWater_TankCanGetAddWater_addValue() {
+        Tank tank = new Tank(3, 1);
+        int result = tank.addWater(1);
+        Assert.assertEquals(1, result);
+
+    }
+    }
