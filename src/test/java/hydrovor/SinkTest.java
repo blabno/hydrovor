2d8228282c138b0f634803f794c29df71b4115b7
PanMistrzu <mariusz899@gmail.com>
2015-04-08 06:49:55 +0200

    Adding SinkTest.java

--- src/test/java/hydrovor/SinkTest.java
+++ src/test/java/hydrovor/SinkTest.java
@@ -1,5 +1,32 @@
 package hydrovor;
 
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 public class SinkTest {
 
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
+	@Test
+	public void constructor_nullHydrovorGiven_throwException() {
+		exception.expect(IllegalArgumentException.class);
+		new Sink(null);
+	}
+	
+	@Test
+	public void getWater_methodCalled_delegateToHydrovor() {
+		// Given
+		Hydrovor hydrovor = mock(Hydrovor.class);
+		when(hydrovor.getWater()).thenReturn(1);
+		Sink sink = new Sink(hydrovor);
+		// When
+		int result = sink.getWater();
+		// Then
+		assertEquals(1, result);
+	}
+	
 }
